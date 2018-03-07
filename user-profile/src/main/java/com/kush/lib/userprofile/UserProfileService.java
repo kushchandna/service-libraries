1a4f45822f0d86ff87e9f7a82632e5a1c34914de
Kush Chandna <kushchandna@gmail.com>
2018-03-07 19:12:32 +0530

    created empty library project user-profile

---
+++ user-profile/src/main/java/com/kush/lib/userprofile/UserProfileService.java
@@ -0,0 +1,6 @@
+package com.kush.lib.userprofile;
+
+import com.kush.lib.service.server.BaseService;
+
+public class UserProfileService extends BaseService {
+}
---
+++ user-profile/src/test/java/com/kush/lib/userprofile/sample/SampleApplication.java
@@ -0,0 +1,35 @@
+package com.kush.lib.userprofile.sample;
+
+import com.kush.lib.persistence.helpers.InMemoryPersistor;
+import com.kush.lib.service.server.ApplicationServer;
+import com.kush.lib.service.server.Context;
+import com.kush.lib.service.server.ContextBuilder;
+import com.kush.lib.service.server.authentication.credential.UserCredential;
+import com.kush.lib.service.server.local.LocalApplicationServer;
+import com.kush.lib.userprofile.UserProfileService;
+import com.kush.utils.id.Identifier;
+import com.kush.utils.id.SequentialIdGenerator;
+
+public class SampleApplication {
+
+    public static void main(String[] args) throws Exception {
+        ApplicationServer server = new LocalApplicationServer();
+        server.registerService(UserProfileService.class);
+        Context context = ContextBuilder.create()
+            .withPersistor(UserCredential.class, new InMemoryUserCredentialPersistor())
+            .build();
+        server.start(context);
+    }
+
+    private static final class InMemoryUserCredentialPersistor extends InMemoryPersistor<UserCredential> {
+
+        private InMemoryUserCredentialPersistor() {
+            super(new SequentialIdGenerator());
+        }
+
+        @Override
+        protected UserCredential createPersistableObject(Identifier id, UserCredential reference) {
+            return new UserCredential(id, reference.getUser(), reference.getCredential());
+        }
+    }
+}
