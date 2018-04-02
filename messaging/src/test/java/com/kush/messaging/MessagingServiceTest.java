270716bd057ea171935639e18e7c60348ba3ad2d
Kush Chandna <kushchandna@gmail.com>
2018-04-02 19:35:09 +0530

    renamed test class

--- messaging/src/test/java/com/kush/messaging/MessagingServiceTest.java
+++ messaging/src/test/java/com/kush/messaging/MessagingServiceTest.java
@@ -41,7 +41,7 @@ import com.kush.messaging.persistors.MessagePersistor;
 import com.kush.messaging.push.MessageHandler;
 import com.kush.messaging.push.SignalSpaceProvider;
 import com.kush.messaging.services.MessagingService;
-import com.kush.service.TestApplicationServer;
+import com.kush.service.TestApplicationEnvironment;
 import com.kush.utils.id.Identifier;
 import com.kush.utils.signaling.DefaultSignalEmitterFactory;
 import com.kush.utils.signaling.SignalEmitterFactory;
@@ -53,7 +53,7 @@ public class MessagingServiceTest {
     private static final Clock CLOCK = Clock.fixed(CURRENT_TIME, CURRENT_ZONE);
 
     @Rule
-    public TestApplicationServer server = new TestApplicationServer(5) {
+    public TestApplicationEnvironment testEnv = new TestApplicationEnvironment(5) {
 
         private ExecutorService emitterExecutor;
 
@@ -88,33 +88,33 @@ public class MessagingServiceTest {
     public void beforeEachTest() throws Exception {
         MockitoAnnotations.initMocks(this);
         messagingService = new MessagingService();
-        server.registerService(messagingService);
+        testEnv.registerService(messagingService);
     }
 
     @Test
     public void sendMessage() throws Exception {
-        User[] users = server.getUsers();
+        User[] users = testEnv.getUsers();
         User user1 = users[0];
         User user2 = users[1];
 
         String testMessage = "Test Message";
 
-        server.runAuthenticatedOperation(user1, () -> {
+        testEnv.runAuthenticatedOperation(user1, () -> {
             sendTestMessage(user2, testMessage);
         });
 
-        server.runAuthenticatedOperation(user2, () -> {
+        testEnv.runAuthenticatedOperation(user2, () -> {
             validateMessageReceived(user1, testMessage);
         });
 
-        server.runAuthenticatedOperation(user1, () -> {
+        testEnv.runAuthenticatedOperation(user1, () -> {
             validateMessageSent(user1, testMessage);
         });
     }
 
     @Test
     public void pushNotification() throws Exception {
-        User[] users = server.getUsers();
+        User[] users = testEnv.getUsers();
         User user1 = users[0];
         User user2 = users[1];
         User user3 = users[2];
@@ -127,30 +127,30 @@ public class MessagingServiceTest {
         TestMessageHandler messageHandlerUser1 = new TestMessageHandler();
         TestMessageHandler messageHandlerUser2 = new TestMessageHandler();
 
-        server.runAuthenticatedOperation(user1, () -> {
+        testEnv.runAuthenticatedOperation(user1, () -> {
             messagingService.registerMessageHandler(messageHandlerUser1);
             messageHandlerUser1.setRegistered(true);
         });
 
-        server.runAuthenticatedOperation(user2, () -> {
+        testEnv.runAuthenticatedOperation(user2, () -> {
             messagingService.registerMessageHandler(messageHandlerUser2);
             messageHandlerUser2.setRegistered(true);
         });
 
         CountDownLatch latchUser1 = messageHandlerUser1.resetLatch();
         CountDownLatch latchUser2 = messageHandlerUser2.resetLatch();
-        server.runAuthenticatedOperation(user3, () -> {
+        testEnv.runAuthenticatedOperation(user3, () -> {
             sendTestMessage(user1, testMessage1);
             messageHandlerUser1.setExpectedMessage(testMessage1, user3);
             sendTestMessage(user2, testMessage2);
             messageHandlerUser2.setExpectedMessage(testMessage2, user3);
         });
 
-        server.runAuthenticatedOperation(user1, () -> {
+        testEnv.runAuthenticatedOperation(user1, () -> {
             validateMessageReceived(user3, testMessage1);
         });
 
-        server.runAuthenticatedOperation(user2, () -> {
+        testEnv.runAuthenticatedOperation(user2, () -> {
             validateMessageReceived(user3, testMessage2);
         });
 
@@ -164,23 +164,23 @@ public class MessagingServiceTest {
             fail("Message handler for user 2 didn't got any call");
         }
 
-        server.runAuthenticatedOperation(user1, () -> {
+        testEnv.runAuthenticatedOperation(user1, () -> {
             messagingService.unregisterMessageHandler(messageHandlerUser1);
             messageHandlerUser1.setRegistered(false);
         });
 
         latchUser2 = messageHandlerUser2.resetLatch();
-        server.runAuthenticatedOperation(user3, () -> {
+        testEnv.runAuthenticatedOperation(user3, () -> {
             sendTestMessage(user1, testMessage3);
             sendTestMessage(user2, testMessage4);
             messageHandlerUser2.setExpectedMessage(testMessage4, user3);
         });
 
-        server.runAuthenticatedOperation(user1, () -> {
+        testEnv.runAuthenticatedOperation(user1, () -> {
             validateMessageReceived(user3, testMessage1, testMessage3);
         });
 
-        server.runAuthenticatedOperation(user2, () -> {
+        testEnv.runAuthenticatedOperation(user2, () -> {
             validateMessageReceived(user3, testMessage2, testMessage4);
         });
 
