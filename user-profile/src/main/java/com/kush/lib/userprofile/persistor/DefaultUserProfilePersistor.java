68d6825af1654ff51f78f5760ae89fe6ce73bc30
Kush Chandna <kushchandna@gmail.com>
2018-04-04 20:08:11 +0530

    removed unused method

--- user-profile/src/main/java/com/kush/lib/userprofile/persistor/DefaultUserProfilePersistor.java
+++ user-profile/src/main/java/com/kush/lib/userprofile/persistor/DefaultUserProfilePersistor.java
@@ -2,18 +2,11 @@ package com.kush.lib.userprofile.persistor;
 
 import com.kush.lib.persistence.api.DelegatingPersistor;
 import com.kush.lib.persistence.api.Persistor;
-import com.kush.lib.persistence.api.PersistorOperationFailedException;
-import com.kush.lib.service.remoting.auth.User;
 import com.kush.lib.userprofile.profile.UserProfile;
 
 public class DefaultUserProfilePersistor extends DelegatingPersistor<UserProfile> implements UserProfilePersistor {
 
     public DefaultUserProfilePersistor(Persistor<UserProfile> delegate) {
         super(delegate);
-    }
-
-    @Override
-    public UserProfile getProfileForUser(User user) throws PersistorOperationFailedException {
-        return null;
     }
 }
