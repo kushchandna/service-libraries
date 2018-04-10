21a5dc1c487cd9cb449b17b776f1a5aa2232eed1
Kush Chandna <kushchandna@gmail.com>
2018-04-10 14:57:49 +0530

    minor refactoring

--- user-profile/src/main/java/com/kush/lib/profile/fields/Fields.java
+++ user-profile/src/main/java/com/kush/lib/profile/fields/Fields.java
@@ -6,26 +6,30 @@ import java.time.LocalDateTime;
 public class Fields {
 
     public static FieldBuilder<String> createTextFieldBuilder() {
-        return new FieldBuilder<>();
+        return createFieldBuilder();
     }
 
     public static FieldBuilder<Integer> createIntegerFieldBuilder() {
-        return new FieldBuilder<>();
+        return createFieldBuilder();
     }
 
     public static FieldBuilder<Double> createNumericFieldBuilder() {
-        return new FieldBuilder<>();
+        return createFieldBuilder();
     }
 
     public static FieldBuilder<Boolean> createBooleanFieldBuilder() {
-        return new FieldBuilder<>();
+        return createFieldBuilder();
     }
 
     public static FieldBuilder<LocalDate> createDateFieldBuilder() {
-        return new FieldBuilder<>();
+        return createFieldBuilder();
     }
 
     public static FieldBuilder<LocalDateTime> createDateTimeFieldBuilder() {
+        return createFieldBuilder();
+    }
+
+    private static <T> FieldBuilder<T> createFieldBuilder() {
         return new FieldBuilder<>();
     }
 }
