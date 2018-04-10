2e7d2a234b2b43f7ad303babf88a60985b631072
Kush Chandna <kushchandna@gmail.com>
2018-04-10 18:24:03 +0530

    added google api key and its loader

---
+++ location/src/main/java/com/kush/lib/location/google/GoogleApiKeyLoader.java
@@ -0,0 +1,18 @@
+package com.kush.lib.location.google;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class GoogleApiKeyLoader {
+
+    private static final String API_FILE_NAME = "google_places_api_key.dat";
+    private static final Path API_FILE_PATH = Paths.get("src", "main", "resources", API_FILE_NAME);
+
+    public static String loadKey() throws IOException {
+        List<String> allLines = Files.readAllLines(API_FILE_PATH);
+        return allLines.get(0);
+    }
+}
