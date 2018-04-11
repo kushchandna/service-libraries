14051e136d3a4e5836ba183f551ac0afa656eda9
Kush <Kush@KUSH>
2018-04-12 00:55:00 +0530

    using generic http classes

--- location/src/main/java/com/kush/lib/location/google/GoogleApi.java
+++ location/src/main/java/com/kush/lib/location/google/GoogleApi.java
@@ -1,47 +1,16 @@
 package com.kush.lib.location.google;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Iterator;
-import java.util.Map;
-
 public class GoogleApi {
 
-    private static final String DEFAULT_ENCODING = "UTF-8";
-    private static final String API_URL = "https://maps.googleapis.com/maps/api";
+    private static final String BASE_URL = "https://maps.googleapis.com/maps/api";
 
-    public static String getUrlWithParameters(String apiName, Map<String, Object> parameters) {
-        String queryString = buildQueryString(parameters);
-        String queryUrl = API_URL + "/" + apiName;
-        if (!queryString.isEmpty()) {
-            queryUrl += ("?" + queryString);
-        }
-        return queryUrl;
+    private static final String PLACE_API_NAME = "places/textsearch/json";
+
+    public static String getPlacesApiUrl() {
+        return getApiUrl(PLACE_API_NAME);
     }
 
-    private static String buildQueryString(final Map<String, Object> map) {
-        try {
-            final Iterator<Map.Entry<String, Object>> it = map.entrySet().iterator();
-            final StringBuilder sb = new StringBuilder(map.size() * 8);
-            while (it.hasNext()) {
-                final Map.Entry<String, Object> entry = it.next();
-                final String key = entry.getKey();
-                if (key != null) {
-                    sb.append(URLEncoder.encode(key, DEFAULT_ENCODING));
-                    sb.append("=");
-                    final Object value = entry.getValue();
-                    final String valueAsString = value != null ? URLEncoder.encode(value.toString(), DEFAULT_ENCODING) : "";
-                    sb.append(valueAsString);
-                    if (it.hasNext()) {
-                        sb.append("&");
-                    }
-                } else {
-                    assert false : String.format("Null key in query map: %s", map.entrySet());
-                }
-            }
-            return sb.toString();
-        } catch (final UnsupportedEncodingException e) {
-            throw new UnsupportedOperationException(e);
-        }
+    private static String getApiUrl(String apiName) {
+        return BASE_URL + "/" + apiName;
     }
 }
--- location/src/main/java/com/kush/lib/location/google/GooglePoweredPlaceFinder.java
+++ location/src/main/java/com/kush/lib/location/google/GooglePoweredPlaceFinder.java
@@ -2,28 +2,29 @@ package com.kush.lib.location.google;
 
 import java.io.IOException;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-
 import com.google.common.collect.ImmutableMap;
 import com.kush.lib.location.api.Place;
 import com.kush.lib.location.api.PlaceFinder;
+import com.kush.utils.commons.adapters.StringAdapter;
+import com.kush.utils.http.HttpClient;
 
 public class GooglePoweredPlaceFinder implements PlaceFinder {
+
+    public GooglePoweredPlaceFinder() {
+    }
 
     @Override
     public Place find(String text) throws PlaceSearchFailedException {
         try {
             String key = GoogleApiKeyLoader.loadKey();
-            String urlWithParameters = GoogleApi.getUrlWithParameters("places/textsearch/json", ImmutableMap.of(
+            String placesApiUrl = GoogleApi.getPlacesApiUrl();
+
+            StringAdapter adapter = null;
+            HttpClient httpClient = new HttpClient(placesApiUrl, adapter);
+            Object result = httpClient.getObject(Object.class, ImmutableMap.of(
                     "query", text,
                     "key", key));
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpGet request = new HttpGet(urlWithParameters);
-            HttpResponse response = client.execute(request);
-            response.toString();
+            result.toString();
             return null;
         } catch (IOException e) {
             throw new PlaceSearchFailedException(e.getMessage(), e);
