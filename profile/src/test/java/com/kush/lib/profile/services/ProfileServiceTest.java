56cb73e26b18993a795ca1fa3c138b83b67f008e
Kush Chandna <kushchandna@gmail.com>
2018-04-27 19:26:32 +0530

    removed empty test method

--- profile/src/test/java/com/kush/lib/profile/services/ProfileServiceTest.java
+++ profile/src/test/java/com/kush/lib/profile/services/ProfileServiceTest.java
@@ -106,10 +106,6 @@ public class ProfileServiceTest extends BaseServiceTest {
         });
     }
 
-    @Test
-    public void testName() throws Exception {
-    }
-
     private void setupProfilePersistor() {
         Persistor<Profile> delegate = InMemoryPersistor.forType(Profile.class);
         addToContext(ProfilePersistor.class, new DefaultProfilePersistor(delegate));
