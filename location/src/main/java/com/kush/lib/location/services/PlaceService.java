1ed0aaec7b52dea419494781895bd87333d02d32
Kush <Kush@KUSH>
2018-05-04 22:19:43 +0530

    added service annotations

--- location/src/main/java/com/kush/lib/location/services/PlaceService.java
+++ location/src/main/java/com/kush/lib/location/services/PlaceService.java
@@ -4,9 +4,13 @@ import com.kush.lib.location.api.Place;
 import com.kush.lib.location.api.PlaceFinder;
 import com.kush.lib.location.google.PlaceSearchFailedException;
 import com.kush.lib.service.server.BaseService;
+import com.kush.lib.service.server.annotations.Service;
+import com.kush.lib.service.server.annotations.ServiceMethod;
 
+@Service(name = "Place")
 public class PlaceService extends BaseService {
 
+    @ServiceMethod(name = "Find")
     public Place findPlace(String text) throws PlaceSearchFailedException {
         PlaceFinder placeFinder = getInstance(PlaceFinder.class);
         return placeFinder.find(text);
