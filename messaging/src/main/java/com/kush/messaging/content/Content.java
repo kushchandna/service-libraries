ca362a60ce43c2b8ca3c859d80fba04f75b940dd
Kush <Kush@KUSH>
2018-05-05 04:08:53 +0530

    making things exportable

--- messaging/src/main/java/com/kush/messaging/content/Content.java
+++ messaging/src/main/java/com/kush/messaging/content/Content.java
@@ -2,5 +2,8 @@ package com.kush.messaging.content;
 
 import java.io.Serializable;
 
+import com.kush.lib.service.server.annotations.Exportable;
+
+@Exportable
 public interface Content extends Serializable {
 }
--- messaging/src/main/java/com/kush/messaging/destination/Destination.java
+++ messaging/src/main/java/com/kush/messaging/destination/Destination.java
@@ -1,7 +1,9 @@
 package com.kush.messaging.destination;
 
+import com.kush.lib.service.server.annotations.Exportable;
 import com.kush.utils.id.Identifier;
 
+@Exportable
 public interface Destination {
 
     DestinationType getType();
