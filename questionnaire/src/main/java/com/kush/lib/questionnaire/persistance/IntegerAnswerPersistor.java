bae3484eda47ef997e47afcd34fe687e7b3cc25b
Kush Chandna <kush.chandna@iongroup.com>
2019-02-25 14:07:56 +0530

    deleting again

--- questionnaire/src/main/java/com/kush/lib/questionnaire/persistance/IntegerAnswerPersistor.java
+++
@@ -1,7 +0,0 @@
-package com.kush.lib.questionnaire.persistance;
-
-import com.kush.lib.persistence.api.Persistor;
-import com.kush.lib.questionnaire.ans.IntegerAnswer;
-
-public interface IntegerAnswerPersistor extends Persistor<IntegerAnswer> {
-}
