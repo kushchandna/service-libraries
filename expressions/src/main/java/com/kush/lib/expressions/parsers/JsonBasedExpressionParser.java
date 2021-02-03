a6b890f92290779ba39e09338877f2251b58c1f4
kushchandna <kushchandna@10.101.140.123>
2021-02-03 17:56:49 +0530

    added jackson dependency for writting json based expression parser

---
+++ expressions/src/main/java/com/kush/lib/expressions/parsers/JsonBasedExpressionParser.java
@@ -0,0 +1,13 @@
+package com.kush.lib.expressions.parsers;
+
+import com.kush.lib.expressions.Expression;
+import com.kush.lib.expressions.ExpressionParser;
+import com.kush.lib.expressions.ExpressionParsingFailedException;
+
+public class JsonBasedExpressionParser implements ExpressionParser<String> {
+
+    @Override
+    public Expression parse(String input) throws ExpressionParsingFailedException {
+        return null;
+    }
+}
