0a90a481f472a31343f3652ec587f7fba40c3343
kushchandna <kushchandna@10.101.141.81>
2021-02-09 13:22:29 +0530

    added method in type to get type from class

--- expressions/src/main/java/com/kush/lib/expressions/Type.java
+++ expressions/src/main/java/com/kush/lib/expressions/Type.java
@@ -1,10 +1,33 @@
 package com.kush.lib.expressions;
 
+import java.util.Optional;
+
 public enum Type {
     STRING,
     DOUBLE,
     FLOAT,
     LONG,
     INTEGER,
-    BOOLEAN
+    BOOLEAN;
+
+    public static Optional<Type> fromClass(Class<?> clazz) {
+        return Optional.ofNullable(getType(clazz));
+    }
+
+    private static Type getType(Class<?> clazz) {
+        if (clazz == String.class) {
+            return STRING;
+        } else if (clazz == int.class || clazz == Integer.class) {
+            return INTEGER;
+        } else if (clazz == long.class || clazz == Long.class) {
+            return LONG;
+        } else if (clazz == double.class || clazz == Double.class) {
+            return DOUBLE;
+        } else if (clazz == float.class || clazz == Float.class) {
+            return FLOAT;
+        } else if (clazz == boolean.class || clazz == Boolean.class) {
+            return BOOLEAN;
+        }
+        return null;
+    }
 }
