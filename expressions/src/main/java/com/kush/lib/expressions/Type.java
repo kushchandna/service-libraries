850ae08e3ea2e4a6f38b34c60c3ed4c19d89e0f6
kushchandna <kushchandna@10.101.140.147>
2021-02-11 20:14:02 +0530

    refactoring

--- expressions/src/main/java/com/kush/lib/expressions/Type.java
+++ expressions/src/main/java/com/kush/lib/expressions/Type.java
@@ -1,32 +1,34 @@
 package com.kush.lib.expressions;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public enum Type {
-    STRING,
-    DOUBLE,
-    FLOAT,
-    LONG,
-    INTEGER,
-    BOOLEAN;
+    STRING(String.class),
+    DOUBLE(double.class, Double.class),
+    FLOAT(float.class, Float.class),
+    LONG(long.class, Long.class),
+    INTEGER(int.class, Integer.class),
+    BOOLEAN(boolean.class, Boolean.class);
 
-    public static Optional<Type> fromClass(Class<?> clazz) {
+    private List<Class<?>> javaClasses;
+
+    private Type(Class<?>... javaClasses) {
+        this.javaClasses = Arrays.asList(javaClasses);
+    }
+
+    public static Optional<Type> forClass(Class<?> clazz) {
         return Optional.ofNullable(getType(clazz));
     }
 
     private static Type getType(Class<?> clazz) {
-        if (clazz == String.class) {
-            return STRING;
-        } else if (clazz == int.class || clazz == Integer.class) {
-            return INTEGER;
-        } else if (clazz == long.class || clazz == Long.class) {
-            return LONG;
-        } else if (clazz == double.class || clazz == Double.class) {
-            return DOUBLE;
-        } else if (clazz == float.class || clazz == Float.class) {
-            return FLOAT;
-        } else if (clazz == boolean.class || clazz == Boolean.class) {
-            return BOOLEAN;
+        for (Type type : values()) {
+            for (Class<?> typeClass : type.javaClasses) {
+                if (typeClass.isAssignableFrom(clazz)) {
+                    return type;
+                }
+            }
         }
         return null;
     }
