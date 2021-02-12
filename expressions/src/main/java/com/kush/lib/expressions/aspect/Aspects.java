1e71e5a3239e4f5217e08230352e99b4340e5402
kushchandna <kushchandna@KUSH>
2021-02-13 00:48:37 +0530

    added annotation

--- expressions/src/main/java/com/kush/lib/expressions/aspect/Aspects.java
+++ expressions/src/main/java/com/kush/lib/expressions/aspect/Aspects.java
@@ -2,10 +2,12 @@ package com.kush.lib.expressions.aspect;
 
 import java.util.Map;
 
+import com.kush.lib.expressions.types.ImpactedByAutoBoxing;
 import com.kush.lib.expressions.types.Type;
 
 public class Aspects {
 
+    @ImpactedByAutoBoxing
     public static Aspect<Map<String, Object>> mapBased(Map<String, Type> fieldTypes) {
         return new MapBasedAspect(fieldTypes);
     }
