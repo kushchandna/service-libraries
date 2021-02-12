a61c22a59d2e57b37aa1580f4d7b0a8bf375188c
kushchandna <kushchandna@KUSH>
2021-02-13 00:47:17 +0530

    corrected text

--- expressions/src/main/java/com/kush/lib/expressions/types/ImpactedByAutoBoxing.java
+++ expressions/src/main/java/com/kush/lib/expressions/types/ImpactedByAutoBoxing.java
@@ -5,7 +5,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 import java.lang.annotation.Retention;
 
 /**
- * This annotation is a marker annotation
+ * This is a marker annotation
  */
 @Retention(SOURCE)
 public @interface ImpactedByAutoBoxing {
