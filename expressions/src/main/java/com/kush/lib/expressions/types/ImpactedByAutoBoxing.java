558f5c23f5429556138590bcf5d1e7c1391d55ed
kushchandna <kushchandna@KUSH>
2021-02-13 00:41:11 +0530

    added annotation to mark methods or classes which generally will involve auto-boxing and hence should not be used in performance intensive operations

---
+++ expressions/src/main/java/com/kush/lib/expressions/types/ImpactedByAutoBoxing.java
@@ -0,0 +1,12 @@
+package com.kush.lib.expressions.types;
+
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+
+/**
+ * This annotation is a marker annotation
+ */
+@Retention(SOURCE)
+public @interface ImpactedByAutoBoxing {
+}
