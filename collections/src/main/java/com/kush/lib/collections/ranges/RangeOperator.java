b5c8598d69f64834f0413f7b6c6006c0ff0f9be1
kushchandna <kushchandna@KUSH>
2021-03-21 16:15:30 +0530

    added support for operations on ranges

--- collections/src/main/java/com/kush/lib/collections/ranges/RangeOperator.java
+++ collections/src/main/java/com/kush/lib/collections/ranges/RangeOperator.java
@@ -4,7 +4,8 @@ import static java.util.Comparator.nullsFirst;
 import static java.util.Comparator.nullsLast;
 
 import java.util.Comparator;
-import java.util.Optional;
+
+import com.kush.lib.collections.utils.NullableOptional;
 
 public class RangeOperator<T> {
 
@@ -22,29 +23,73 @@ public class RangeOperator<T> {
         return range.isPointRange();
     }
 
-    public boolean isInRange(Range<T> range, T key) {
-        if (range.getStart().isPresent()) {
-            int resultStart = comparator.compare(key, range.getStart().get());
-            if (resultStart < 0) {
-                return false;
-            }
-            if (resultStart == 0 && !range.isStartInclusive()) {
-                return false;
-            }
+    public boolean isEmpty(Range<T> range) {
+        if (range.getStart().isAbsent() || range.getEnd().isAbsent()) {
+            return false;
         }
-        if (range.getEnd().isPresent()) {
-            int resultEnd = comparator.compare(key, range.getEnd().get());
-            if (resultEnd < 0) {
-                return false;
-            }
-            if (resultEnd == 0 && !range.isEndInclusive()) {
-                return false;
-            }
+        int comparision = comparator.compare(range.getStart().get(), range.getEnd().get());
+        if (comparision == 0) {
+            return range.isStartInclusive() && range.isEndInclusive();
+        }
+        return comparision > 0;
+    }
+
+    public boolean isInRange(Range<T> range, T key) {
+        if (isEmpty(range)) {
+            return false;
+        }
+        int startComparision = startComparator().compare(range.getStart(), NullableOptional.of(key));
+        if (startComparision > 0) {
+            return false;
+        }
+        if (startComparision == 0 && !range.isStartInclusive()) {
+            return false;
+        }
+        int endComparision = endComparator().compare(NullableOptional.of(key), range.getEnd());
+        if (endComparision > 0) {
+            return false;
+        }
+        if (endComparision == 0 && !range.isEndInclusive()) {
+            return false;
         }
         return true;
     }
 
-    public Optional<Range<T>> intersect(Range<T> range1, Range<T> range2) {
-        return null;
+    private Comparator<NullableOptional<T>> startComparator() {
+        return new RangePointComparator<>(comparator, false);
+    }
+
+    private Comparator<NullableOptional<T>> endComparator() {
+        return new RangePointComparator<>(comparator, true);
+    }
+
+    private static class RangePointComparator<T> implements Comparator<NullableOptional<T>> {
+
+        private final Comparator<T> valueComparator;
+        private final boolean isEmptyHigh;
+
+        public RangePointComparator(Comparator<T> valueComparator, boolean isEmptyHigh) {
+            this.valueComparator = valueComparator;
+            this.isEmptyHigh = isEmptyHigh;
+        }
+
+        @Override
+        public int compare(NullableOptional<T> o1, NullableOptional<T> o2) {
+            if (o1.isAbsent() && o2.isAbsent()) {
+                return 0;
+            }
+            if (o1.isAbsent()) {
+                // isEmptyHigh true - o1 is larger than o2
+                // isEmptyHigh false - o1 is smaller than o2
+                return isEmptyHigh ? 1 : -1;
+            }
+            if (o2.isAbsent()) {
+                // isEmptyHigh true - o2 is larger than o1
+                // isEmptyHigh false - o2 is smaller than o1
+                return isEmptyHigh ? -1 : 1;
+            }
+            // both o1 and o2 contain a value
+            return valueComparator.compare(o1.get(), o2.get());
+        }
     }
 }
